97d7d7561b4c9ecd566cf1ddfd7ac65dabfb8228
Olena Hlotova <lenaglotova21@gmail.com>
2021-10-14 01:58:54 +0000

    spring

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/service/TwitterService.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/service/TwitterService.java
@@ -5,8 +5,10 @@ import ca.jrvs.apps.twitter.model.Tweet;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
+@org.springframework.stereotype.Service
 public class TwitterService implements Service{
 
   private CrdDao dao;
@@ -17,6 +19,7 @@ public class TwitterService implements Service{
   private static final float LAT_MIN = -90;
   private static final float LAT_MAX = 90;
 
+  @Autowired
   public TwitterService(CrdDao dao) {
     this.dao = dao;
   }
