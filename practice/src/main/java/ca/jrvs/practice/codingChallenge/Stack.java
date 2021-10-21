5fa4541b4233d0a6832b769854537a408fb75716
Olena Hlotova <lenaglotova21@gmail.com>
2021-10-21 17:30:44 +0000

    Stack using 1/2 queue

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/Stack.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/Stack.java
@@ -9,7 +9,11 @@ import java.util.Queue;
  * Stack using queue
  */
 
+/**
+ * 2 queues
+ */
 class Stack {
+
   Queue<Integer> q1;
   Queue<Integer> q2;
 
@@ -23,7 +27,7 @@ class Stack {
   }
 
   public int pop() {
-    while (q1.size()>1) {
+    while (q1.size() > 1) {
       int i = q1.poll();
       q2.add(i);
     }
@@ -44,43 +48,86 @@ class Stack {
     q2 = new LinkedList<>();
     return n;
   }
+
   public boolean empty() {
     return q1.isEmpty();
   }
-  public String toString(){
+
+  public String toString() {
     return q1.toString();
   }
   public static void main(String[] args) {
     Stack obj = new Stack();
-//    System.out.println(obj);
+    System.out.println(obj);
     obj.push(1);
-//    System.out.println(obj);
+    System.out.println(obj);
     obj.push(2);
-//    System.out.println(obj);
+    System.out.println(obj);
     int p = obj.top();
-//    System.out.println(obj + " top "+ p);
+    System.out.println(obj + " top "+ p);
     int l = obj.pop();
-    int l2 = obj.pop();
 
     System.out.println(l + " popped");
 
     boolean b = obj.empty();
     System.out.println(b+" empty "+obj);
   }
+}
 
-  class StackOneQueue {
-    Queue<Integer> q1;
-    public StackOneQueue() {
-      this.q1 = new LinkedList<Integer>();
+/**
+ * 1 queue
+ */
+class StackOneQueue {
+  Queue<Integer> q1;
+  public StackOneQueue() {
+    this.q1 = new LinkedList<Integer>();
+  }
+  public void push(int x) {
+    q1.add(x);
+  }
+  public boolean empty() {
+    return q1.isEmpty();
+  }
+  public String toString(){
+    return q1.toString();
+  }
+
+  public int pop() {
+    int size = q1.size();
+    for (int i = 1; i<size; i++){
+      q1.add(q1.poll());
     }
-    public void push(int x) {
-      q1.add(x);
+    int result = q1.poll();
+    return result;
+  }
+
+  public int top() {
+    int size = q1.size();
+    for (int i = 1; i<size; i++){
+      q1.add(q1.poll());
+
     }
-    public boolean empty() {
-      return q1.isEmpty();
-    }
-    public String toString(){
-      return q1.toString();
-    }
+    int result = q1.peek();
+    q1.add(result);
+    q1.poll();
+    return result;
+  }
+
+
+  public static void main(String[] args) {
+    StackOneQueue obj = new StackOneQueue();
+    System.out.println(obj);
+    obj.push(1);
+    System.out.println(obj);
+    obj.push(2);
+    System.out.println(obj);
+    int p = obj.top();
+    System.out.println(obj + " top "+ p);
+    int l = obj.pop();
+
+    System.out.println(l + " popped");
+
+    boolean b = obj.empty();
+    System.out.println(b+" empty "+obj);
   }
 }
