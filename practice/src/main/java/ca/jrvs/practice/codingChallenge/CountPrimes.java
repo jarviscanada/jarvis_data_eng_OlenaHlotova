f053a2721adc8c7be94c493fa7f618197881cd98
Olena Hlotova <lenaglotova21@gmail.com>
2021-10-29 22:27:37 +0000

    Count Primes

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/CountPrimes.java
@@ -0,0 +1,36 @@
+package ca.jrvs.practice.codingChallenge;
+
+/**
+ * https://www.notion.so/jarvisdev/Count-Primes-ce436e4baa1d4f638321291954534550
+ * Count Primes
+ * O()
+ */
+
+public class CountPrimes {
+
+  public int count(int n) {
+    if (n <= 2)
+      return 0;
+
+    // init an array to track prime numbers
+    boolean[] primes = new boolean[n];
+    for (int i = 2; i < n; i++)
+      primes[i] = true;
+
+    for (int i = 2; i <= Math.sqrt(n - 1); i++) {
+      // or for (int i = 2; i <= n-1; i++) {
+      if (primes[i]) {
+        for (int j = i + i; j < n; j += i)
+          primes[j] = false;
+      }
+    }
+
+    int count = 0;
+    for (int i = 2; i < n; i++) {
+      if (primes[i])
+        count++;
+    }
+    return count;
+  }
+
+}
