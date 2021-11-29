ee18d4cf0dcbc1b99fd616ae7f45597b692cd928
Olena Hlotova <lenaglotova21@gmail.com>
2021-11-29 15:02:52 +0000

    springtest

--- springboot/src/main/java/ca/jrvs/apps/trading/dao/ResourceNotFoundException.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/ResourceNotFoundException.java
@@ -1,2 +1,7 @@
-package ca.jrvs.apps.trading.dao;public class ResourceNotFoundException {
+package ca.jrvs.apps.trading.dao;
+
+public class ResourceNotFoundException extends Exception {
+  public ResourceNotFoundException(String errorMessage) {
+    super(errorMessage);
+  }
 }
